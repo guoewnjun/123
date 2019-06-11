67eeac4ef1f4aaad4b659b686c1f99804e555638
zibuyu213 <m18335835389@163.com>
2019-06-11 16:38:40 +0800

    意见模板初版

---
+++ src/containers/OperationsAndCenter/WorkOrder/OpinionTemplate.jsx
@@ -0,0 +1,251 @@
+import React, {Component} from "react";
+import {
+    Button, Form, Select, Radio, Table, Row, Col, DatePicker, Input, Spin, Pagination, Switch, Badge,
+} from "antd";
+import {HttpClientImmidIot} from "../../../common/HttpClientImmidIot";
+
+
+const FormItem = Form.Item;
+const RangePicker = DatePicker.RangePicker;
+
+class OpinionTemplate extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false, //表格加载状态
+            AlarmRecord: [
+            ], //表格数据
+            pageNum: 1, //当前页
+            pageSize: 10, //一页多少数据
+            total: 1, //数据总条数
+            otherParams: {
+                warningDisposeStatus: -1, //处理状态按钮组当前值
+                warningType: null,//报警类型
+                parkingSpaceNo: null,//泊位编号
+            },
+        };
+    }
+
+    // 分页变化
+    onPageChange(pageNum) {
+        this.setState({
+            pageNum: pageNum,
+        }, () => {
+            this.loadData(pageNum, this.state.pageSize, this.state.otherParams)
+        });
+    }
+
+    // 分页大小变化
+    onShowSizeChange(pageNum, pageSize) {
+        this.setState({
+            pageNum,
+            pageSize
+        }, () => {
+            this.loadData(pageNum, pageSize, this.state.otherParams)
+        });
+    }
+
+    // 筛选参数
+    filterOtherParams(otherParams) {
+        let params = {};
+        for (let item in otherParams) {
+            if (otherParams[item] || item === "warningDisposeStatus") {
+                params[item] = otherParams[item]
+            }
+        }
+        return params
+    }
+
+    //加载数据
+    loadData(pageNum = 1, pageSize = 10, otherParams) {
+        this.setState({
+            loading: true
+        });
+        let params = {
+            pageNum: pageNum,
+            pageSize: pageSize,
+            ...otherParams
+        };
+        params = this.filterOtherParams(params);
+        HttpClientImmidIot.query("/OperationsAndCenter/WorkOrder/ComplainWorkOrder", "GET", params, this.handleQueryData.bind(this))
+    }
+
+
+    // loadData回调函数
+    handleQueryData(d, type) {
+        const data = d.data;
+        if (type === HttpClientImmidIot.requestSuccess) {
+            this.setState({
+                AlarmRecord: data.list,
+                total: data.total
+            })
+        } else {
+            //失败----做除了报错之外的操作
+        }
+        this.setState({
+            loading: false,
+        })
+    }
+
+       //查询按钮
+    handleQuery() {
+        const pageSize = this.state.pageSize;
+        this.setState({
+            pageNum: 1
+        });
+        this.loadData(1, pageSize, this.state.otherParams)
+    }
+
+    //重置
+    handleReset() {
+        this.props.form.resetFields();
+        this.setState({
+            pageNum: 1, //当前页
+            pageSize: 10, //一页多少数据
+            otherParams: {
+                warningDisposeStatus: -1, //处理状态按钮组当前值
+                startTime: null,
+                endTime: null,
+                warningType: null,
+                parkingSpaceNo: null,
+            }
+        }, () => {
+            this.loadData()
+        });
+    }
+
+    //添加按钮
+ AddButton() {
+     const pageSize = this.state.pageSize;
+     this.setState({
+         pageNum: 1
+     });
+     this.loadData(1, pageSize, this.state.otherParams)
+ }
+
+
+    // 组件挂载之前
+    componentWillMount() {
+    }
+
+    // 组件挂载后
+    componentDidMount() {
+      this.loadData();
+    }
+
+    // 组件卸载之前
+    componentWillUnmount() {
+
+    }
+
+
+    render() {
+
+    const {loading, pageNum, pageSize, AlarmRecord, total, otherParams: {warningDisposeStatus}} = this.state;
+
+    const { Option } = Select;
+
+    function handleChange(value) {
+      console.log(`selected ${value}`);
+    }
+
+        const columns = [
+              {
+                  title: "意见标题",
+                  dataIndex: "workorderHeadline",
+                  render: (value) => value || "--",
+              },{
+                  title: "意见分类",
+                  dataIndex: "comfrom",
+                  render: (value) => value || "--",
+              }, {
+                  title: "添加时间",
+                  dataIndex: "creationTime",
+                  render: (value) => value || "--",
+              }, ,{
+                  title: "操作",
+                  dataIndex: "Operation",
+                  render: (value) => value || "--",
+              },
+          ];
+
+        const {getFieldDecorator} = this.props.form;
+
+        const formItemLayout = {
+            labelCol: {span: 5},
+            wrapperCol: {span: 19},
+        };
+
+
+
+        return (
+             <div className="page">
+                <div className="page-header">
+                    意见模板
+                </div>
+                <div className="page-content">
+                    <Form className="queryForm">
+                        <Row gutter={48}>
+                            <Col span={8}>
+                                <FormItem label="意见标题" {...formItemLayout}>
+                                    {getFieldDecorator("workorderHeadline")(
+                                        <Input placeholder="请输入" onChange={(e) => {
+                                            this.state.otherParams.workorderHeadline = e.target.value;
+                                        }}/>
+                                    )}
+                                </FormItem>
+                            </Col>
+                            <Col span={8}>
+                                <FormItem label="意见分类" {...formItemLayout}>
+                                    {getFieldDecorator("comfrom")(
+                                        <Select defaultValue="all"  onChange={handleChange}>
+                                            <Option value="all">全部</Option>
+                                            <Option value="classify1">分类1</Option>
+                                            <Option value="classify2">分类2</Option>
+                                        </Select>
+                                    )}
+                                </FormItem>
+                            </Col>
+                            <Col span={8} style={{textAlign: "right"}}>
+                                <Button type="primary" onClick={this.handleQuery.bind(this)}>查询</Button>
+                                <Button style={{marginLeft: "20px"}}
+                                        onClick={this.handleReset.bind(this)}>重置</Button>
+                                <Button style={{marginLeft: "20px"}} type="primary" onClick={this.AddButton.bind(this)}>添加</Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                    <Spin tip="加载中.." spinning={loading}>
+
+                        <Table
+                            style={{marginTop: "20px"}}
+                            columns={columns}
+                            dataSource={AlarmRecord}
+                        />
+                        {/*分页*/}
+                        {AlarmRecord.length > 0 ? (
+                            <div>
+                                <div className="table_pagination_total">共{total}条</div>
+                                <Pagination
+                                    className="table_pagination"
+                                    showSizeChanger
+                                    showQuickJumper
+                                    total={total}
+                                    current={pageNum}
+                                    pageSize={pageSize}
+                                    onChange={this.onPageChange.bind(this)}
+                                    onShowSizeChange={this.onShowSizeChange.bind(this)}
+                                />
+                                <div style={{clear: "both"}}></div>
+                            </div>
+                        ) : ""}
+                    </Spin>
+                </div>
+             </div>
+
+
+            );
+    }
+}
+
+const WrapperAbnormalParkingAlarm = Form.create()(OpinionTemplate);
+export default WrapperAbnormalParkingAlarm;
